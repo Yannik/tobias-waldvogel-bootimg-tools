8d5ba017e3b9eb003b58cfa6d7bb92f7e6105148
Yannik Sembritzki <yannik@sembritzki.me>
2016-04-15 16:23:27 +0200

    Cleanup

--- bootimg/build2.sh
+++
@@ -1,8 +0,0 @@
-export CROSS=/home/yannik/Downloads/android-ndk-r11c/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/bin/arm-linux-androideabi-
-#export CROSS=/home/yannik/Downloads/android-ndk-r11c/toolchains/x86-4.9/prebuilt/linux-x86_64/bin/i686-linux-android-
-#export CROSS=
-export SYSROOT=/home/yannik/Downloads/android-ndk-r11c/platforms/android-21/arch-arm
-export CC=${CROSS}gcc
-export LD=${CROSS}ld
-export AS=${CROSS}as
-make ARCH=arm build
--- libsepol/build2.sh
+++
@@ -1,9 +0,0 @@
-#export CROSS=~/Downloads/android-ndk-r11c/toolchains/arm-linux-androideabi-4.9/prebuilt/linux-x86_64/bin/arm-linux-androideabi-
-#export CROSS=/home/yannik/Downloads/android-ndk-r11c/toolchains/x86-4.9/prebuilt/linux-x86_64/bin/i686-linux-android-
-#export SYSROOT=/home/yannik/Downloads/android-ndk-r11c/platforms/android-21/arch-arm
-export CC=${CROSS}gcc
-export LD=${CROSS}ld
-export AS=${CROSS}as
-export AR=${CROSS}ar
-#make ARCH=arm build
-make
